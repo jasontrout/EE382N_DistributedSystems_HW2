d153996cf00ec42d154126fc93550f8257de1497
Jason Trout <jsnetrout@gmail.com>
2019-10-08 23:21:08 -0500

    Server progress

--- Server.java
+++ Server.java
@@ -16,6 +16,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.Map;
@@ -33,202 +34,37 @@ public class Server implements IServer {
   // Data interface
   class DataInterface {
 
-
-    class Order {
-
-      private int orderId;
-      private String productName;
-      private int productQuantity;
-
-      public Order(int orderId, String productName, int productQuantity) {
-        this.orderId = orderId;
-        this.productName = productName;
-        this.productQuantity = productQuantity;
-      }
-
-      public int getOrderId() {
-        return orderId;
-      }
-
-      public String getProductName() {
-        return productName;
-      }
-
-      public int getProductQuantity() {
-        return productQuantity;
-      }
-
-      @Override
-      public boolean equals(Object o) {
-        if (!(o instanceof Order)) return false;
-        Order rhsOrder = (Order)o;
-        if (orderId == rhsOrder.getOrderId() && 
-            productName.equals(rhsOrder.getProductName()) &&
-            productQuantity == rhsOrder.getProductQuantity()) {
-          return true;
-        }
-        return false;
-      }
-
-      @Override
-      public int hashCode() {
-        return Objects.hash(orderId, productName, productQuantity);
-      }
-
-
-    } 
-
-    private Map<String, Integer> productMap = new TreeMap<>();
-    private Map<String, Map<Integer, Order>> userNameToOrdersMap = new TreeMap<>();
-    private Map<Integer, String> orderIdToUsernameMap = new TreeMap<>();
-    private int orderIdCounter = 1;
+    private Map<String, Integer> nameToSeatNumMap = new HashMap<>();
 
     public DataInterface() { }
 
-    // Load inventory from a file.
-    public synchronized void loadInventoryFromFile(String fileName) throws FileNotFoundException {
-      File file = new File(fileName);
-      Scanner sc = new Scanner(file);
-      while (sc.hasNextLine()) { 
-        String name = sc.next(); 
-    	  int quantity = sc.nextInt();
-	      productMap.put(name, quantity);
-      }
-      sc.close();
+    // reserve
+    public synchronized String reserve(String name) {
+      return "";
     }
 
-    // Purchase.
-    public synchronized String purchase(String username, String productName, int productQuantity) {
-      StringBuilder sb = new StringBuilder();
-      if (productQuantity < 1) {
-        sb.append("At least one of the product must be purchased for an order");
-      } else if (!productMap.containsKey(productName)) {
-        sb.append("Not Available - We do not set this product");
-      } else {
-        int productQuantityRemaining = productMap.get(productName);
-        if (productQuantity > productQuantityRemaining) {
-          sb.append("Not Available - Not enough items");
-        } else {
-          int orderId = orderIdCounter++;
-          Order order = new Order(orderId, productName, productQuantity);
-          if (!userNameToOrdersMap.containsKey(username)) {
-            userNameToOrdersMap.put(username, new TreeMap<>());
-          }
-          userNameToOrdersMap.get(username).put(orderId, order);
-          orderIdToUsernameMap.put(orderId, username);
-          productMap.put(productName, productQuantityRemaining - productQuantity);
-          sb.append("Your order has been placed, ").append(orderId).append(" ").append(username).append(" ").append(productName).append(" ").append(productQuantity);
-        }
-      }
-      return sb.append("\n").toString();
+    // bookSeat
+    public synchronized String bookSeat(String name, int seatNum) {
+      return "";
     }
 
-    // Cancel.
-    public synchronized String cancel(int orderId) {
-      StringBuilder sb = new StringBuilder();
-      if (!orderIdToUsernameMap.containsKey(orderId)) {
-        sb.append(orderId).append(" not found, no such order");
-      } else {
-        String username = orderIdToUsernameMap.get(orderId);
-        Order order = userNameToOrdersMap.get(username).get(orderId);
-        String productName = order.getProductName();
-        int productQuantity = order.getProductQuantity();
-        int productQuantityRemaining = productMap.get(productName);
-        productMap.put(productName, productQuantityRemaining + productQuantity);
-        orderIdToUsernameMap.remove(orderId);
-        userNameToOrdersMap.get(username).remove(orderId);
-        sb.append("Order ").append(orderId).append(" is canceled");
-      }
-      return sb.append("\n").toString();
+    // search
+    public synchronized String search(String name) {
+      return "";
     }
 
-    // Search.
-    public synchronized String search(String username) {
-      StringBuilder sb = new StringBuilder();
-      Map<Integer, Order> orderIdToOrderMap = userNameToOrdersMap.get(username);
-      if (orderIdToOrderMap != null && orderIdToOrderMap.size() > 0) {
-        for (int orderId : orderIdToOrderMap.keySet()) {
-          Order order = orderIdToOrderMap.get(orderId);
-          String productName = order.getProductName();
-          int productQuantity = order.getProductQuantity();
-          sb.append(orderId).append(", ").append(productName).append(", ").append(productQuantity).append("\n");
-        }
-      } else {
-        sb.append("No order found for ").append(username).append("\n");
-      }
-      return sb.toString();
-    }
-
-    // List.
-    public synchronized String list() { 
-      StringBuilder sb = new StringBuilder();
-      productMap.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed().thenComparing(Map.Entry.comparingByKey()))
-        .forEach((e) -> {
-        sb.append(e.getKey()).append(" ").append(e.getValue()).append("\n");
-      });
-      return sb.toString();
-    }
+    // delete
+    public synchronized String delete(String name) {
+      return "";
+    } 
 
   }
 
   private DataInterface dataInterface = new DataInterface();
 
 
-  // UDP Server Thread
-  class UdpServerThread extends Thread {
-    private IServer server;
-    private DatagramSocket socket;
-    private byte[] buf = new byte[1024];
-    private int port;
-    private boolean running ;
-
-    public UdpServerThread(int port, IServer server) {
-      this.port = port;
-      this.server = server;
-    }
-
-    public void run() {
-      try {
-        running = true;
-        socket = new DatagramSocket(port);
-        System.out.println("UDP server started. Listening on " + socket.getLocalAddress() + ":" + port);
-        while (running) {
-          try { 
-            DatagramPacket srcPacket = new DatagramPacket(buf, buf.length);
-	          socket.receive(srcPacket);
-
-	          InetAddress srcAddress = srcPacket.getAddress();
-	          int srcPort = srcPacket.getPort();
-
-
-            String command = new String(srcPacket.getData(), 0, srcPacket.getLength());
-            String response = server.executeCommand(command);
-            
-            if (response != null) {
-              byte[] responseBytes = response.getBytes();
-              DatagramPacket responsePacket = new DatagramPacket(responseBytes, responseBytes.length, srcAddress, srcPort);
-              socket.send(responsePacket);
-            }
-          } catch (Exception ex) {
-	          ex.printStackTrace();
-          }
-        }
-      } catch (Exception e) {
-        System.out.println("Error in UdpServerThread.");
-        e.printStackTrace();
-      }
-    }
-
-    @Override
-    public void interrupt() {
-      super.interrupt();
-      running = false;
-      socket.close();
-    }
-  }
-
-  // TCP Client Thread
-  class TcpClientThread extends Thread {
+  // TCP Command Client Thread
+  class TcpCommandClientThread extends Thread {
 
     private int clientId;
     private ITcpServerThread tcpServerThread;
@@ -237,7 +73,7 @@ public class Server implements IServer {
     private boolean running;
 
 
-    public TcpClientThread(int clientId, Socket socket, ITcpServerThread tcpServerThread, IServer server) {
+    public TcpCommandClientThread(int clientId, Socket socket, ITcpServerThread tcpServerThread, IServer server) {
       this.clientId = clientId;
       this.socket = socket;
       this.tcpServerThread = tcpServerThread;
@@ -275,7 +111,7 @@ public class Server implements IServer {
           tcpServerThread.notifyClientDisconnected(clientId, socket);
         }
       } catch (Exception e) {
-        System.out.println("Error in TcpClientThread");
+        System.out.println("Error in TcpCommandClientThread");
         e.printStackTrace();
       }
     }
@@ -305,7 +141,7 @@ public class Server implements IServer {
     private int port;
     private boolean running;
     private int clientIdCounter = 1;
-    private Map<Integer, TcpClientThread> clientIdToClientThreadMap = new TreeMap<>();
+    private Map<Integer, TcpCommandClientThread> clientIdToClientThreadMap = new TreeMap<>();
    
 
     public TcpServerThread(int port, IServer server) {
@@ -320,7 +156,7 @@ public class Server implements IServer {
         System.out.println("TCP server started. Listening on " + socket.getLocalSocketAddress());
         while (running) {
           Socket clientSocket = socket.accept();
-          TcpClientThread tcpClientThread = new TcpClientThread(clientIdCounter, clientSocket, this, server);
+          TcpCommandClientThread tcpClientThread = new TcpCommandClientThread(clientIdCounter, clientSocket, this, server);
           clientIdToClientThreadMap.put(clientIdCounter, tcpClientThread);
           tcpClientThread.start();
           clientIdCounter++;
@@ -370,69 +206,75 @@ public class Server implements IServer {
   // Executes command from client and returns a response. Returns null if an unknown command is provided.
   public String executeCommand(String command) {
     String[] tokens = command.split(" ");
-    if (tokens[0].equals("purchase") && tokens.length == 4) {
-      String username = tokens[1];
-      String productName = tokens[2];
-      int quantity = parseInt(tokens[3]);
-      return dataInterface.purchase(username, productName, quantity);
-    } else if (tokens[0].equals("cancel") && tokens.length == 2) {
-      int orderId = parseInt(tokens[1]);
-      return dataInterface.cancel(orderId);
+    if (tokens[0].equals("reserve") && tokens.length == 2) {
+      String name = tokens[1];
+      return dataInterface.reserve(name);
+    } else if (tokens[0].equals("bookSeat") && tokens.length == 3) {
+      String name = tokens[1];
+      int seatNum = parseInt(tokens[2]);
+      return dataInterface.bookSeat(name, seatNum);
     } else if (tokens[0].equals("search") && tokens.length == 2) {
-      String username = tokens[1];
-      return dataInterface.search(username);
-    } else if (tokens[0].equals("list") && tokens.length == 1) {
-      return dataInterface.list();
+      String name = tokens[1];
+      return dataInterface.search(name);
+    } else if (tokens[0].equals("delete") && tokens.length == 2) {
+      String name = tokens[1];
+      return dataInterface.delete(name);
     } else {
       return null;
     }
   }
 
-  public void start(int tcpPort, int udpPort, String fileName) {
+  public void start(int myID, int numServer, int numSeat, List<String> replicaHostStrings) {
 
     try {
 
-      // Load inventory file.
-      dataInterface.loadInventoryFromFile(fileName);
+      int numReplicaConnectionsRemaining = numServer;
+
+      // Start the TCP clients to the replica servers.
+      for (String replicaHostString : replicaHostStrings) {
+        System.out.println("Waiting on connection acceptance from " + replicaHostString + ". [" + numReplicaConnectionsRemaining + " replica connections remaining before accepting client commands].");
+        numReplicaConnectionsRemaining--;
+      }
 
       // Start the TCP server.
-      TcpServerThread tcpServerThread = new TcpServerThread(tcpPort, this);
-      tcpServerThread.start();
-
-      // Start the UDP server.
-      UdpServerThread udpServerThread = new UdpServerThread(udpPort, this);
-      udpServerThread.start();
+      //TcpServerThread tcpServerThread = new TcpServerThread(tcpPort, this);
+      //tcpServerThread.start();
 
       // Wait for the threads to finish.
-      tcpServerThread.join();
-      udpServerThread.join();
+      //tcpServerThread.join();
 
-
-    } catch (InterruptedException e) {
-      System.out.println("Main thread interrupted.");
+    //} catch (InterruptedException e) {
+    //  System.out.println("Main thread interrupted.");
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
   public static void main (String[] args) {
-    int tcpPort;
-    int udpPort;
-    if (args.length != 3) {
-      System.out.println("ERROR: Provide 3 arguments");
-      System.out.println("\t(1) <tcpPort>: the port number for TCP connection");
-      System.out.println("\t(2) <udpPort>: the port number for UDP connection");
-      System.out.println("\t(3) <file>: the file of inventory");
 
-      System.exit(-1);
+    Scanner sc = new Scanner(System.in);
+    int myID = sc.nextInt();
+    int numServer = sc.nextInt();
+    int numSeat = sc.nextInt();
+
+    List<String> replicaHostStrings = new ArrayList<>();
+    
+    for (int i = 0; i < numServer; i++) {
+      replicaHostStrings.add(sc.next());
     }
 
-    tcpPort = Integer.parseInt(args[0]);
-    udpPort = Integer.parseInt(args[1]);
-    String fileName = args[2];
+    System.out.println("");
+    System.out.println("###############################################");
+    System.out.println("SERVER " + myID + " is starting!");
+    System.out.println("Number of replica servers: " + numServer);
+    System.out.println("Number of seats: " + numSeat);
+    for (String replicaHostString : replicaHostStrings) {
+      System.out.println("Replica Server: " + replicaHostString);
+    }
+    System.out.println("###############################################");
 
     Server server = new Server();
-    server.start(tcpPort, udpPort, fileName);
+    server.start(myID, numServer, numSeat, replicaHostStrings);
 
   }
 }
